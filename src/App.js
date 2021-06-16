90475c89fe2d62bbc621f322b9dccf982652b5d8
zen-machina <rojosudo@gmail.com>
2021-06-16 15:56:37 -0700

    added pokedex loop and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Pokecard id={4} name="Charmander" type="fire" exp={62} />
+                <Pokedex />
             </div>
         );
     }
